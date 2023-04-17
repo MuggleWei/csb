bda5f7d047ffbc5e5b7d5ae7ee22ae0a7e96eb4c
Muggle Wei <mugglewei@gmail.com>
2023-04-17 23:57:01 +0800

    update gitignore

--- pyinstaller_pkg.bat
+++ pyinstaller_pkg.bat
@@ -6,8 +6,8 @@ pip install -r requirements.dev.txt
 
 pyinstaller -F hpb\main.py --distpath dist\hpb -n hpb
 xcopy hpb\etc\ dist\hpb\etc\ /Y /S
-xcopy scripts\ dist\hpb\scripts\ /Y /S
-xcopy README.md dist\hpb\README.md /Y /F
-xcopy README_cn.md dist\hpb\README_cn.md /Y /F
+xcopy share\ dist\hpb\share\ /Y /S
+xcopy README.md dist\hpb\ /Y
+xcopy README_cn.md dist\hpb\ /Y
 
 call venv\Scripts\deactivate.bat
